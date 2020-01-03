85521dad0bed56851baaeb2bf9cee39d5e55866e
Kim C Raath <kcr2@ad.rice.edu>
2020-01-02 18:51:12 -0600

    Updates to match

--- Test/Figures_Code_Paper-YZ.R
+++ Test/Figures_Code_Paper-YZ.R
@@ -455,18 +455,18 @@ lowFP1<-32
 upFP1<-128
 
 WCO<-CoFEScoherency(Data[,1],Data[,2],low.period=low.period,up.period=up.period,
-                    low.fp = lowFP1,up.fp = upFP1, Phase_diff = TRUE)
+                    low.fp = lowFP1,up.fp = upFP1, Phase_diff = TRUE, date = date1)
 
 # -------- PLOTS  ----------
-plot.CoFESWaveWCO(WCO,pE=5)
+plot.CoFESWaveWCO(WCO,pE=5, horizons.label = FALSE)
 
 
 # Computation of coherency of (x, y)
 WCO_recon<-CoFEScoherency(periodic_waveL2E_2$recon_L2E$series$x.r,
                           periodic_waveL2E$recon_L2E$series$x.r,
-                          low.fp = lowFP1,up.fp = upFP1, Phase_diff = TRUE)
+                          low.fp = lowFP1,up.fp = upFP1, Phase_diff = TRUE, date = date1)
 
-plot.CoFESWaveWCO(WCO_recon,pE=5)
+plot.CoFESWaveWCO(WCO_recon,pE=5, horizons.label = FALSE)
 
 
 # ----- Computation of Partial Coherency of Water , Energy (controlling for SPY) ---- #
@@ -476,7 +476,7 @@ Data_thresh <- cbind(periodic_waveL2E_2$recon_L2E$series$x.r,periodic_waveL2E$re
 X<-matrix(Data_thresh,T,3)
 W<-CoFESmpcoherency(X, coher.type='part',index.p=index.p,
                  low.period=low.period,up.period=up.period,
-                 low.fp = lowFP1,up.fp = upFP1, Phase_diff = TRUE)
+                 low.fp = lowFP1,up.fp = upFP1, Phase_diff = TRUE, date = date1)
 
 plot.CoFESWaveWCO(W,pE=5)
 
